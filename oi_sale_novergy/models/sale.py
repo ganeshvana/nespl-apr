84b387001a599422723527e39f28c6e94c07a4d9
Kiruthikapoorani A <krithipoorani92@gmail.com>
2022-04-29 14:33:29 +0530

    sale

--- oi_sale_novergy/models/sale.py
+++ oi_sale_novergy/models/sale.py
@@ -7,5 +7,20 @@ class product_pricelist(models.Model):
 
     partner_id = fields.Many2one('res.partner',string="Partner", copy=False)
     
+class SO(models.Model):
+    _inherit = "sale.order"
+    
+    bank = fields.Many2one('res.partner.bank')
+    
+class SOL(models.Model):
+    _inherit = "sale.order.line"
+    
+    pi = fields.Boolean("PI")
+    
+class Bank(models.Model):
+    _inherit = "res.bank"
+    
+    micr = fields.Char("MICR Code")
+    
 
 
