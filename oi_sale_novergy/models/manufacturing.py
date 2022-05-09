53d133427127eeec2249de28f3edc7d04cd77cb2
Kiruthikapoorani A <krithipoorani92@gmail.com>
2022-05-09 22:20:05 +0530

    nespl

--- oi_sale_novergy/models/manufacturing.py
+++ oi_sale_novergy/models/manufacturing.py
@@ -3,7 +3,6 @@ from odoo import api, fields, models, tools, _
 class mrp_bom(models.Model):
     _inherit = "mrp.bom"
 
-
     partner_id = fields.Many2one('res.partner',string="Partner", copy=False)
     
 
